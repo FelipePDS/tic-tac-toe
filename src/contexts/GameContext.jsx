537af698718b36c09219f4dc3a043238bb2ba175
FelipePDS <felipepdasilva66@gmail.com>
2021-06-26 20:40:50 -0300

    add methods for winning player and tied round

--- src/contexts/GameContext.jsx
+++ src/contexts/GameContext.jsx
@@ -8,41 +8,102 @@ const GameContext = createContext();
 export function GameContextProvider({ children }) {
   const { 
     players,
+    playerTurn,
     restartPlayers
   } = usePlayerContext();
 
-  const { restartBoardLines } = useCellsContext()
+  const { 
+    boardLines,
+    restartBoardLines
+  } = useCellsContext();
+
+  const winningPossibilities = [
+    ['a1', 'a2', 'a3'],
+    ['b1', 'b2', 'b3'],
+    ['c1', 'c2', 'c3'],
+    ['a1', 'b1', 'c1'],
+    ['a2', 'b2', 'c2'],
+    ['a3', 'b3', 'c3'],
+    ['a1', 'b2', 'c3'],
+    ['a3', 'b2', 'c1']
+  ];
 
   const scoreProps = {
     times: 0,
     circle: 0
-  }
-
-  const winningPossibilities = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
+  };
 
   const [score, setScore] = useState(scoreProps);
   const [winningPlayerOfRound, setWinningPlayerOfRound] = useState('');
-  const [endOfRound, setEndOfRound] = useState(false);
-
-  // Verificar jogadas do jogador
+  const [tiedRound, setTiedRound] = useState(false);
+  const [endRound, setEndRound] = useState(false);
 
   function startNewRound() {
     restartPlayers();
     restartBoardLines();
+
+    setTimeout(() => {
+      setEndRound(false);
+    }, 2000);
+  }
+
+  function declareWinner(player) {
+    let newScore  = score;
+    newScore[player]++;
+
+    setWinningPlayerOfRound(player);
+    setScore(newScore);
+    setTiedRound(false);
+    setEndRound(true);
+    startNewRound();
+  }
+
+  function declareTie() {
+    setWinningPlayerOfRound('');
+    setTiedRound(true);
+    setEndRound(true);
+    startNewRound();
+  }
+  
+  function toggleRoundConditions() {
+    const player = playerTurn === 'times' ? 'circle' : 'times';
+
+    const playerIsWinner = winningPossibilities.map(winningPossibilitie => (
+
+      players[player].plays.map(play => (
+        winningPossibilitie.filter(possibilitie => 
+          possibilitie === play
+        ).length === 1 ? play : null
+      ))
+      .filter(play => play !== null)
+      .length === 3
+
+    )).find(playsComparedToWinning => playsComparedToWinning === true);
+
+    if (playerIsWinner) {
+      declareWinner(player);
+    }
+
+    let markedCellCounter = 0;
+
+    boardLines.forEach(([ line ]) => {
+      line.cells.forEach(cell => {
+        cell.isMarked && markedCellCounter++;
+      });
+    });
+
+    if (markedCellCounter === 9) {
+      declareTie();
+    }
   }
   
   return (
     <GameContext.Provider value={{
-      score
+      score,
+      toggleRoundConditions,
+      winningPlayerOfRound,
+      tiedRound,
+      endRound
     }}>
       {children}
     </GameContext.Provider>
