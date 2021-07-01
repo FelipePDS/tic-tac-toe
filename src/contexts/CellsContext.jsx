0acb70c53056b853902a11e4bac07e0f2b6e099c
FelipePDS <felipepdasilva66@gmail.com>
2021-07-01 20:57:21 -0300

    remove handleClick

--- src/contexts/CellsContext.jsx
+++ src/contexts/CellsContext.jsx
@@ -1,32 +1,11 @@
 import React, { createContext, useContext, useState } from 'react';
 
-import { usePlayerContext } from './PlayersContext';
 import { boardLinesProps } from '../utils/cellsUtil';
 
 const CellsContext = createContext();
 
 export function CellsContextProvider({ children }) {
-  const { 
-    playerTurn,
-    togglePlayerTurn,
-    addPlayerPlay
-  } = usePlayerContext();
-
   const [boardLines, setBoardLines] = useState(boardLinesProps);
-
-  function handleClick(lineIndex, cellIndex) {
-    const cellIsMarked = boardLines[lineIndex][0].cells[cellIndex].isMarked;
-
-    if (!cellIsMarked) {
-      let newBoardLine = boardLines;
-      newBoardLine[lineIndex][0].cells[cellIndex].isMarked = true;
-      newBoardLine[lineIndex][0].cells[cellIndex].playerMarkedCell = playerTurn;
-
-      setBoardLines(newBoardLine);
-      addPlayerPlay(boardLines[lineIndex][0].cells[cellIndex].id, playerTurn);
-      togglePlayerTurn();
-    }
-  }
 
   function restartBoardLines() {
     boardLinesProps.forEach(([ line ]) => {
@@ -44,7 +23,7 @@ export function CellsContextProvider({ children }) {
   return (
     <CellsContext.Provider value={{
       boardLines,
-      handleClick,
+      setBoardLines,
       restartBoardLines
     }}>
       {children}
